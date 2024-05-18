19bb1fc5472810aa95a678447df7d458ff02eabd
Serhii Krasovskyi <chokorik@gmail.com>
2024-05-18 23:50:48 +0300

    complit

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,10 @@
 import HttpError from "../helpers/HttpError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import * as fs from "node:fs/promises";
+import path from "node:path";
+import gravatar from "gravatar";
+import Jimp from "jimp";
 
 import User from "../models/user.js";
 import {
@@ -24,8 +28,13 @@ export async function registerUser(req, res, next) {
     }
 
     const passwordHash = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
 
-    const user = await User.create({ email, password: passwordHash });
+    const user = await User.create({
+      email,
+      password: passwordHash,
+      avatarURL,
+    });
 
     res
       .status(201)
@@ -59,7 +68,7 @@ export async function loginUser(req, res, next) {
     const token = jwt.sign(
       { id: user._id, email: user.email, subscription: user.subscription },
       process.env.JWT_SECRET,
-      { expiresIn: 60 * 60 }
+      { expiresIn: 60 * 120 }
     );
 
     await User.findByIdAndUpdate(user._id, { token });
@@ -88,9 +97,37 @@ export async function currentUser(req, res) {
   res.send({ email, subscription });
 }
 
+export async function usersAvatar(req, res, next) {
+  try {
+    const resize = await Jimp.read(req.file.path);
+    await resize.resize(250, 250);
+    resize.write(req.file.path);
+
+    await fs.rename(
+      req.file.path,
+      path.resolve("public/avatars", req.file.filename)
+    );
+
+    const user = await User.findOneAndUpdate(
+      req.user._id,
+      { avatarURL: req.file.filename },
+      { new: true }
+    );
+
+    if (user === null) {
+      return res.status(401).send({ message: "Not authorized" });
+    }
+
+    res.status(200).send({ avatarURL: user.avatarURL });
+  } catch (error) {
+    next(error);
+  }
+}
+
 export default {
   registerUser,
   loginUser,
   logout,
   currentUser,
+  usersAvatar,
 };
